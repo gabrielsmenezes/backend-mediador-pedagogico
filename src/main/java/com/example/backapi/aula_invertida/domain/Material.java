ad2ebc5279d442cebcad7a39c8eec25d0933cbad
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-08-28 20:36:54 -0400

    Alterando a forma de geracao do id

--- src/main/java/com/example/backapi/aula_invertida/domain/Material.java
+++ src/main/java/com/example/backapi/aula_invertida/domain/Material.java
@@ -11,7 +11,7 @@ import java.util.Objects;
 public class Material implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column
--- src/main/java/com/example/backapi/aviso/domain/Aviso.java
+++ src/main/java/com/example/backapi/aviso/domain/Aviso.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class Aviso implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
