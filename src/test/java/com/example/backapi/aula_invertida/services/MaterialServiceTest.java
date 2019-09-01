1c613eeebd2457fbce016d649f232aa60ddcc35f
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-09-01 17:11:17 +0000

    Feature/deploy heroku

--- src/test/java/com/example/backapi/aula_invertida/services/MaterialServiceTest.java
+++ src/test/java/com/example/backapi/aula_invertida/services/MaterialServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,6 +18,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class MaterialServiceTest {
 
     @Autowired
