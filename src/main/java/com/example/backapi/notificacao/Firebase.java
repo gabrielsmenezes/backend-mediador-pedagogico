181b4f089bf8a949a13993a62b909980cd031030
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-09-11 16:45:00 -0400

    Adicionando a dependencia do firebase

---
+++ src/main/java/com/example/backapi/notificacao/Firebase.java
@@ -0,0 +1,11 @@
+package com.example.backapi.notificacao;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class TestandoFirebase{
+    public void teste(){
+        FirebaseMessaging firebase = FirebaseMessaging.getInstance();
+
+        firebase.
+    }
+}
