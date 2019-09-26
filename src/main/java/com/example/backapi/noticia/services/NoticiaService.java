603eccf7e1c8b0dedb3049dcd6b447a5b628851c
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-09-26 10:10:56 -0400

    Criando validações do tamanho dos campos

--- src/main/java/com/example/backapi/noticia/services/NoticiaService.java
+++ src/main/java/com/example/backapi/noticia/services/NoticiaService.java
@@ -3,6 +3,7 @@ package com.example.backapi.noticia.services;
 import com.example.backapi.noticia.domain.Noticia;
 import com.example.backapi.noticia.domain.NoticiaDTO;
 import com.example.backapi.noticia.repositories.NoticiaRepository;
+import com.example.backapi.utils.exceptions.TamanhoDeCampoExcedente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,11 @@ public class NoticiaService {
     @Autowired
     NoticiaRepository noticiaRepository;
 
-    public NoticiaDTO save(NoticiaDTO noticiaDTO) {
+    public NoticiaDTO save(NoticiaDTO noticiaDTO) throws TamanhoDeCampoExcedente {
+
+        validarTamanhoTitulo(noticiaDTO);
+        validarTamanhoDescricao(noticiaDTO);
+
         Noticia noticia = DTOToNoticia(noticiaDTO);
 
         noticia.setDataDeCriacao(new java.util.Date());
@@ -44,4 +49,17 @@ public class NoticiaService {
         noticia.setNotificavel(noticiaDTO.isNotificavel());
         return noticia;
     }
+
+    private void validarTamanhoDescricao(NoticiaDTO noticiaDTO) throws TamanhoDeCampoExcedente {
+        if (noticiaDTO.getDescricao().length() > 100) {
+            throw new TamanhoDeCampoExcedente("O tamanho do campo descricao é de no máximo 100 caracteres");
+        }
+    }
+
+    private void validarTamanhoTitulo(NoticiaDTO noticiaDTO) throws TamanhoDeCampoExcedente {
+        if (noticiaDTO.getTitulo().length() > 50) {
+            throw new TamanhoDeCampoExcedente("O tamanho do campo titulo é de no máximo 50 caracteres");
+        }
+    }
+
 }
