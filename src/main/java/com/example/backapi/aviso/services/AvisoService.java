6a80b40a7de8a01cd70758bbcbc6cc03d95813d5
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-01 08:47:41 -0400

    Criando notificacao no aviso

--- src/main/java/com/example/backapi/aviso/services/AvisoService.java
+++ src/main/java/com/example/backapi/aviso/services/AvisoService.java
@@ -2,7 +2,9 @@ package com.example.backapi.aviso.services;
 
 import com.example.backapi.aviso.domain.Aviso;
 import com.example.backapi.aviso.repositories.AvisoRepository;
+import com.example.backapi.notificacao.Firebase;
 import com.example.backapi.utils.exceptions.CampoObrigatorio;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -29,13 +32,19 @@ public class AvisoService {
         return entidadeGenerica.orElseThrow(() -> new ObjectNotFoundException(entidadeGenerica.getClass().getName(), "Objeto não encontrado do tipo" + entidadeGenerica.getClass().getName() + " do id " + id) );
     }
 
-    public Aviso save(Aviso aviso) throws CampoObrigatorio, DataIntegrityViolationException {
+    public Aviso save(Aviso aviso) throws CampoObrigatorio, DataIntegrityViolationException, IOException, FirebaseMessagingException {
         if (aviso.getTitulo() == null || (aviso.getDescricao() == null && aviso.getLinks().isEmpty())){
             throw new CampoObrigatorio("Os campos Descricao ou link devem existir");
         }
         Date date=new java.util.Date();
         aviso.setDataDeCriacao(date);
         Aviso resposta = avisoRepository.save(aviso);
+
+
+        Firebase firebase = new Firebase();
+        firebase.sendMessage("avisos", aviso.getTitulo());
+        firebase = null;
+
         return resposta;
 
     }
