f5483ea86b01f4e775a0bccf95db0fb77e9a506c
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-20 07:51:35 -0400

    Evitando com que notificacao seja enviada por testes

--- src/main/java/com/example/backapi/notificacao/listener/MaterialListener.java
+++ src/main/java/com/example/backapi/notificacao/listener/MaterialListener.java
@@ -4,6 +4,7 @@ import com.example.backapi.aula_invertida.domain.material.Material;
 import com.example.backapi.notificacao.model.PushNotificationRequest;
 import com.example.backapi.notificacao.service.PushNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import javax.persistence.PostPersist;
 
@@ -12,9 +13,16 @@ public class MaterialListener {
     @Autowired
     PushNotificationService pushNotificationService;
 
+    @Autowired
+    Environment environment;
+
     @PostPersist
     void posCreate(Material material) {
-        pushNotificationService.sendPushNotification(new PushNotificationRequest(material.getTitulo(), material.getDescricao(), material.getTurma().getChaveDeAcesso()));
-
+        StringBuilder perfil = new StringBuilder();
+        String[] vetor = environment.getActiveProfiles();
+        for (String s : vetor) perfil.append(s);
+        if (!"test".equals(perfil.toString())){
+            pushNotificationService.sendPushNotification(new PushNotificationRequest(material.getTitulo(), material.getDescricao(), material.getTurma().getChaveDeAcesso()));
+        }
     }
 }
