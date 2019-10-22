94b19751406d6304ccd6811711dd23709076423d
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-22 08:32:04 -0400

    Criando testes de CalendarioResource

---
+++ src/test/java/com/example/backapi/calendario/resources/CalendarioResourceTest.java
@@ -0,0 +1,48 @@
+package com.example.backapi.calendario.resources;
+
+import com.example.backapi.calendario.domain.CalendarioDTO;
+import com.example.backapi.utils.exceptions.CampoObrigatorio;
+import com.example.backapi.utils.exceptions.LimiteDeObjetosAtingido;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class CalendarioResourceTest {
+
+    @Autowired
+    CalendarioResource calendarioResource;
+
+    CalendarioDTO calendarioDTO;
+
+    String linkDoCalendario;
+
+    @Before
+    public void setUp() throws Exception {
+        calendarioDTO = new CalendarioDTO();
+        linkDoCalendario = "https://calendar.google.com/calendar/r";
+    }
+
+    @Test
+    public void save() throws LimiteDeObjetosAtingido, CampoObrigatorio {
+        calendarioDTO.setLinkDoCalendario(linkDoCalendario);
+
+        ResponseEntity<CalendarioDTO> resposta = calendarioResource.save(calendarioDTO);
+
+        assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+        assertEquals(linkDoCalendario, resposta.getBody().getLinkDoCalendario());
+
+    }
+
+}
