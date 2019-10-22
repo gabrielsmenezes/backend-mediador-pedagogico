540070301fc87fe31e25906782580124adaae550
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-22 08:04:47 -0400

    Criando metodo de cadastrar calendario

--- src/main/java/com/example/backapi/calendario/resources/CalendarioResource.java
+++ src/main/java/com/example/backapi/calendario/resources/CalendarioResource.java
@@ -1,9 +1,34 @@
 package com.example.backapi.calendario.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.backapi.calendario.domain.Calendario;
+import com.example.backapi.calendario.domain.CalendarioDTO;
+import com.example.backapi.calendario.services.CalendarioService;
+import com.example.backapi.utils.mapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/calendario")
 public class CalendarioResource {
+
+    @Autowired
+    CalendarioService calendarioService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @PostMapping
+    public ResponseEntity<CalendarioDTO> save(@RequestBody CalendarioDTO calendarioDTO){
+        calendarioDTO = modelMapper.modelMapper().map(calendarioService.save(modelMapper.modelMapper().map(calendarioDTO, Calendario.class)), CalendarioDTO.class);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(calendarioDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(calendarioDTO);
+
+    }
+
 }
--- src/main/java/com/example/backapi/calendario/services/CalendarioService.java
+++ src/main/java/com/example/backapi/calendario/services/CalendarioService.java
@@ -1,7 +1,19 @@
 package com.example.backapi.calendario.services;
 
+import com.example.backapi.calendario.domain.Calendario;
+import com.example.backapi.calendario.repositories.CalendarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalendarioService {
+
+    @Autowired
+    CalendarioRepository calendarioRepository;
+
+    public Calendario save(Calendario calendario){
+        return calendarioRepository.save(calendario);
+    }
+
+
 }
