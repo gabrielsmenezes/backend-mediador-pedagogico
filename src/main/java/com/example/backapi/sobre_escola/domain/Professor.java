8ff4763a5ca8f8791fea7216d7368449bce51831
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-23 14:44:01 -0400

    Criando a classe Professor e seus testes

---
+++ src/main/java/com/example/backapi/sobre_escola/domain/Professor.java
@@ -0,0 +1,38 @@
+package com.example.backapi.sobre_escola.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String nome;
+
+    private List<String> disciplinas = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return id.equals(professor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
