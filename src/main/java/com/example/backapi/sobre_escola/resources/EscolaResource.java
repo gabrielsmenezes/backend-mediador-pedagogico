08de8e7c57c22913f499bdde44061123c80f9547
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-23 10:34:42 -0400

    Criando os testes da EscolaService baseados nas estorias

--- src/main/java/com/example/backapi/sobre_escola/resources/EscolaResource.java
+++ src/main/java/com/example/backapi/sobre_escola/resources/EscolaResource.java
@@ -3,6 +3,8 @@ package com.example.backapi.sobre_escola.resources;
 import com.example.backapi.sobre_escola.domain.Escola;
 import com.example.backapi.sobre_escola.domain.EscolaDTO;
 import com.example.backapi.sobre_escola.services.EscolaService;
+import com.example.backapi.utils.exceptions.CampoObrigatorio;
+import com.example.backapi.utils.exceptions.LimiteDeObjetosAtingido;
 import com.example.backapi.utils.mapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +27,7 @@ public class EscolaResource {
     ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity<EscolaDTO> save( @RequestBody EscolaDTO escolaDTO){
+    public ResponseEntity<EscolaDTO> save( @RequestBody EscolaDTO escolaDTO) throws CampoObrigatorio, LimiteDeObjetosAtingido {
         Escola escola = modelMapper.modelMapper().map(escolaDTO, Escola.class);
 
         escolaDTO = modelMapper.modelMapper().map(escolaService.save(escola), EscolaDTO.class);
