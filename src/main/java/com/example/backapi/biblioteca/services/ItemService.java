a15d7f7102fcddcbd9e8a01ff03159b5d6e6c6ba
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-31 10:58:14 -0400

    Removendo import nao usado

--- src/main/java/com/example/backapi/biblioteca/services/ItemService.java
+++ src/main/java/com/example/backapi/biblioteca/services/ItemService.java
@@ -7,8 +7,6 @@ import com.example.backapi.utils.exceptions.ObjetoNaoEncontrado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class ItemService {
 
