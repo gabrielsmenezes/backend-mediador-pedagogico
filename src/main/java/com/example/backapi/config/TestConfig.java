e067f0201fa5484943640af7dcd24a33761bd4d1
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-11-06 18:07:18 -0400

    Trocando perfil

--- src/main/java/com/example/backapi/config/TestConfig.java
+++ src/main/java/com/example/backapi/config/TestConfig.java
@@ -4,12 +4,10 @@ import com.example.backapi.usuario.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
 public class TestConfig {
 
 	@Autowired
