3575b58d01c4ca57fd683ab49d368579d0a01cce
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-11-11 09:42:38 -0400

    liberando frame options

--- src/main/java/com/example/backapi/config/SecurityConfig.java
+++ src/main/java/com/example/backapi/config/SecurityConfig.java
@@ -43,9 +43,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+//		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
-        }
+//        }
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
